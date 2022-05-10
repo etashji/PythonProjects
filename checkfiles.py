0d150abf3e76f0944e52570e6a6d4d7841fba21d
etashji <99293073+etashji@users.noreply.github.com>
2022-05-10 12:21:44 -0400

    filetransferpartone

---
+++ checkfiles.py
@@ -0,0 +1,18 @@
+import tkinter
+from tkinter import *
+
+class ParentWindow(Frame):
+    def __init__ (self, master):
+        Frame.__init__ (self)
+
+        self.master = master
+        self.master.resizable(width = False, height = False)
+        self.master.geometry('{}x{}'.format(700, 400))
+        self.master.title('Learning Tkinter!')
+        self.master.config(bg = 'lightgray')
+
+
+if __name__ == "__main__":
+    root = Tk()
+    App = ParentWindow(root)
+    root.mainloop()
